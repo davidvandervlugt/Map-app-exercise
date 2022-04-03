da7d75a475bdfa6d4c996b89909789c6e0a12969
davidvandervlugt <94963688+davidvandervlugt@users.noreply.github.com>
2022-04-03 22:20:12 +0200

    Adding markers

--- src/mvc/controller.js
+++ src/mvc/controller.js
@@ -1,12 +1,44 @@
+import "./model";
 import layout from "./views/layout";
 import sidebar from "./views/sidebar";
-import map from "./views/map";
+import { renderMap, addMarker } from "./views/map";
+import { mapcoordinates, store } from "./model";
 
+//variables
+let controllerMap = null;
+
+//initialization
 export function init() {
   layout();
   sidebar();
-  map();
+  controllerMap = renderMap(mapcoordinates);
+  renderMapPoints();
+  setAddMarkerEventHandler();
 }
 
-//retrieve geojson from model
-//function for adding data to geojson, and then rerending view
+//controller functions
+function renderMapPoints() {
+  for (let i in store) {
+    addMarker(
+      controllerMap,
+      store[i].geometry.coordinates,
+      store[i].properties.name,
+      store[i].properties.review
+    );
+  }
+}
+
+function submitMarker() {
+  const coordinates = document.getElementById("coordinates").value.split(", ");
+  const place = document.getElementById("place").value;
+  const review = document.getElementById("review").value;
+
+  if (coordinates) addMarker(controllerMap, coordinates, place, review);
+}
+
+function setAddMarkerEventHandler() {
+  const submitButton = document.querySelector(".submit-button");
+  submitButton.addEventListener("click", function () {
+    submitMarker();
+  });
+}
